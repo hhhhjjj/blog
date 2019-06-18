4bca68424c5c0970a721917c74b743fedc976993
HeJames <48619125+hhhhjjj@users.noreply.github.com>
2019-06-18 11:38:22 +0800

    other repo

--- write.py
+++
@@ -1,6 +0,0 @@
-def write(operation):
-    with open(log_path, "a") as f:
-        f.writelines(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
-        f.writelines(",")
-        f.writelines(operation)
-        f.writelines("\n")
